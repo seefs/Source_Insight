718b5e1e1d7b4a3ee235b721fc1e28a1158e1c18
xj_01 <seefs@163.com>
2019-02-08 14:13:35 +0800

    simple_ctrl_b.h

--- node/Simple_CTRL_B.h
+++ node/Simple_CTRL_B.h
@@ -19,3 +19,73 @@ Save:SI\node\Macro_File.h
 
 
 
+常用错误（版本问题）：
+
+---------------------------------
+sess.run(tf.initialize_all_variables())
+sess.run(tf.global_variables_initializer())
+
+---------------------------------
+	
+writer = tf.train.SummaryWriter("logs/", sess.graph)
+writer = tf.summary.FileWriter("logs/", sess.graph)
+
+
+
+---------------------------------
+tf.scalar_summary(layer_name + '/weights', Weights)
+tf.summary.scalar(layer_name + '/weights', Weights)
+
+---------------------------------
+
+tf.merge_all_summaries(layer_name + '/weights', Weights)
+tf.summary.merge_all(layer_name + '/weights', Weights)
+
+
+---------------------------------
+tf.histogram_summary(layer_name + '/weights', Weights)
+tf.summary.histogram(layer_name + '/weights', Weights)
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+
+---------------------------------
+
+
+
+---------------------------------
+
+
+
+
+
+
+
