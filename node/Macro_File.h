5be5a57470e4059416a3f90cbfea01611f14a021
usr_xj <seefs@163.com>
2019-04-28 01:31:06 +0800

    ss

--- node/Macro_File.h
+++ node/Macro_File.h
@@ -1,52 +1,112 @@
 
 // File List [Num][Ca]:
-// 1. bak Cmd
-Save:SI\node\Macro_File.h \[1.1\] bak文件:
-Save:SI\node\Macro_File.h \[1.2\] Cmd:
-Save:SI\node\Macro_File.h \[1.3\] Cmd unzip:
-Save:SI\node\Macro_File.h \[1.4\] file list Test:
-// 2. Macro MacroSBD Test
-Save:SI\node\Macro_File.h \[2.1\] Macro path Set:
-Save:SI\node\Macro_File.h \[2.2\] Test:
-// 3. Project modis (Common)
-Save:SI\node\Macro_File.h \[3.1\] 常用的编辑文件
-Save:SI\node\Macro_File.h \[3.2\] 模拟器
-// 4. Set
-Save:SI\node\Macro_File.h \[4.1\] set:
-Save:SI\node\Macro_File.h \[4.2\] Msg
-Save:SI\node\Macro_File.h \[4.3\] Tmp File
-Save:SI\node\Macro_File.h \[4.4\] rule
-Save:SI\node\Macro_File.h \[4.5\] search
-Save:SI\node\Macro_File.h \[4.6\] Group (不用)
-Save:SI\node\Macro_File.h \[4.7\] version (不稳定, 不要用)
-// 5. Tool
-Save:SI\node\Macro_File.h \[5.1\] Tools:
-Save:SI\node\Macro_File.h \[5.2\] SVN BCmp git SI vim
-// 6. Other
-Save:SI\node\Macro_File.h \[6.1\] 禅道 展讯 MTK
-Save:SI\node\Macro_File.h \[6.2\] 未整理:
-Save:SI\node\Macro_File.h \[6.3\] 未改:
+// 1. 常用笔记
+Save:SI\node\Macro_File.h \[1.1\] work--node
+Save:SI\node\Macro_File.h \[1.2\] Pythons--node
+Save:SI\node\Macro_File.h \[1.3\] Yuyang--node
+// 2. 模拟器
+Save:SI\node\Macro_File.h \[2.1\] 模拟器
+Save:SI\node\Macro_File.h \[2.2\] 未整理 未改
+Save:SI\node\Macro_File.h \[2.3\] bak文件:
+// 3. Cmd
+Save:SI\node\Macro_File.h \[3.1\] Cmd:
+Save:SI\node\Macro_File.h \[3.2\] Cmd unzip:
+Save:SI\node\Macro_File.h \[3.3\] Cmd 对比:
+Save:SI\node\Macro_File.h \[3.4\] Macro path:
+// 4. Test
+Save:SI\node\Macro_File.h \[4.1\] Test:
+Save:SI\node\Macro_File.h \[4.2\] file list Test:
+Save:SI\node\Macro_File.h \[4.3\] Msg
+Save:SI\node\Macro_File.h \[4.4\] Tmp File
+// 5. rule
+Save:SI\node\Macro_File.h \[5.1\] rule
+Save:SI\node\Macro_File.h \[5.2\] search
+Save:SI\node\Macro_File.h \[5.3\] Group (不用)
+Save:SI\node\Macro_File.h \[5.4\] version (不稳定, 不要用)
+// 6. set
+Save:SI\node\Macro_File.h \[6.1\] set:
+Save:SI\node\Macro_File.h \[6.2\] Tools:
+Save:SI\node\Macro_File.h \[6.3\] SVN BCmp git SI vim
+// 7. 其他
+Save:SI\node\Macro_File.h \[7.1\] 禅道 展讯 MTK
+Save:SI\node\Macro_File.h \[7.2\] 参考标号:
+
+
+
+
+
+
+[1.1] work--node
+//1) android:
+Save:SI\node\note\Macro_Note_9820es.h
+//2) MTK SPR:
+Save:SI\node\note\Macro_Note_MTK.h
+Save:SI\node\note\Macro_Note_6531E.h
+[1.2] Pythons--node
+//1) Pythons:
+Save:SI\node\Pythons\Macro_Note_Python.h
+//pythom 网文
+Save:SI\node\Pythons\Macro_Pythons_summary.h
+//Tensorflow 网文
+Save:SI\node\Pythons\Macro_Pythons_tensorflow.h
+[1.3] Yuyang--node
 	
 
 
-/***********************************************************************/
 
-[1.1] bak文件:
+
+
+[2.1] 模拟器
+Save:SI\node\Macro_modis_MTK.h
+Save:SI\node\Macro_modis_6531E.h
+
+
+[2.2] 未整理 未改
+//1) 未整理:
+Save:SI\bak\bak_mtk_lang.h	
+//2) 未改:
+Save:SI\bak\bak_mtk_weiti.h	
+//2) 其他:
+Save:SI\bak\Macro_tmp.h
+
+
+[2.3] bak文件:
 Save:SI\bak\bak_20181114_tmp_B_old.h
 Save:SI\bak\bak_20181123_git.h
-对比:
-Save:SI\bak\bcmp\bmp_107_mk.bat
+	
 
-[1.2] Cmd:
+
+
+
+[3.1] Cmd:
 Save:SI\Cmd\clear_mtk__no_use
 Save:SI\Cmd\dir__no_use
 Save:SI\Cmd\other__no_use
-		
-[1.3] Cmd unzip:
+
+
+[3.2] Cmd unzip:
 Save:SI\Cmd_unzip\6531E\unzip_spr128.bat
 Save:SI\Cmd_unzip\6531E\unzip_spr240_mini.bat
+
+[3.3] Cmd 对比:
+Save:SI\bak\bcmp\bmp_107_mk.bat
+
+
+[3.4] Macro path:
+Save:SI\MacroSBD\sbd_base.em
+
+
+
+[4.1] Test:
+Save:SI\node\Test\Macro_Tmp_Node_Test.h
+Save:SI\node\Test\sbd_Tmp_Show.em
+Save:SI\node\Test\Macro_Tmp_...
+Save:SI\node\Test\...
+Save:SI\node\Test\Tmp_Test.h
 	
-[1.4] file list Test:
+
+
+[4.2] file list Test:
 Save:SI\Cmd\file___no_use\...
 Save:SI\Cmd\file___no_use\dir_getFile...
 Save:SI\Cmd\file___no_use\dir_getFile time.bat
@@ -54,42 +114,8 @@ Save:SI\Cmd\file___no_use\dir_getFile_spr.bat
 Save:SI\Cmd\cmd_getFileList.bat
 Save:SI\Cmd_TXT\si_filelist.h
 
-/***********************************************************************/
 
-[2.1] Macro path Set:
-Save:SI\MacroSBD\sbd_base.em
-
-
-[2.2] Test:
-Save:SI\node\Test\Macro_Tmp_Node_Test.h
-Save:SI\node\Test\Macro_Tmp_...
-Save:SI\node\Test\...
-Save:SI\node\Test\Tmp_Test.h
-
-
-/***********************************************************************/
-
-[3.1] 常用的编辑文件
-Save:SI\node\note\Macro_Note_MTK.h
-Save:SI\node\note\Macro_Note_9820es.h
-Save:SI\node\note\Macro_Note_6531E.h
-	
-[3.2] 模拟器:
-Save:SI\node\Macro_modis_MTK.h
-Save:SI\node\Macro_modis_6531E.h
-
-
-/***********************************************************************/
-
-[4.1] set:
-//1) Common set
-Save:SI\node\Macro_Set.h
-//2) windows system Path
-Save:SI\node\Tmp_Common.h
-//3) cur project
-Save:SI\node\Macro_Set_Note.h
-
-[4.2] Msg
+[4.3] Msg
 //1) Nv Msg
 Save:SI\node\Macro_Val.h
 //2) Pic Path Msg
@@ -98,7 +124,7 @@ Save:SI\node\Show_Pic_dir.h
 Save:SI\node\Simple_CTRL_K.h
 
 
-[4.3] Tmp File
+[4.4] Tmp File
 //1) Tmp File
 Save:SI\node\Simple_CTRL_B.h
 //2) Tmp File
@@ -108,7 +134,7 @@ Save:SI\node\Tmp_Comp_1.h
 Save:SI\node\Tmp_Comp_2.h
 
 
-[4.4] rule
+[5.1] rule
 //1) rule 中文:
 Save:SI\node\rule\Macro_Rule_Han_file.h
 //2) rule LCD+CAM 文件跳转:
@@ -117,25 +143,34 @@ Save:SI\node\rule\Macro_Rule_Key_file_7701.h
 Save:SI\node\rule\Macro_Rule_Key_file_6533.h
 Save:SI\node\rule\Macro_Rule_Key_file_MTK.h
 
-[4.5] search
+[5.2] search
 //1) search 所有宏:
 Save:SI\node\search\Macro_ALL_6531E.h
 //2) search 9820e 所有文件:
 Save:SI\node\search\Macro_ALL_9820e_JAVA.h
 
-[4.6] Group (不用)
+[5.3] Group (不用)
 Save:SI\node\group\Macro_Group_Make_6531E.h
 
-[4.7] version (不稳定, 不要用)
+[5.4] version (不稳定, 不要用)
 Save:SI\node\version\si_version_6531e.h
 
 
-/***********************************************************************/
 
-[5.1] Tools:
+[6.1] set:
+//1) Common set
+Save:SI\node\Macro_Set.h
+//2) windows system Path
+Save:SI\node\Tmp_Common.h
+//3) cur project
+Save:SI\node\Macro_Set_Note.h
+//set--utf8:
+Save:SI\node\Macro_Set.h 63
+
+[6.2] Tools:
 Save:SI\node\note\Macro_Note_Tools.h
 
-[5.2] SVN BCmp git SI vim
+[6.3] SVN BCmp git SI vim
 //1) SVN 配置:
 Save:SI\node\ToolsMsg\Macro_SVN.h
 C:\Users\Administrator\AppData\Roaming\Subversion\config
@@ -153,9 +188,10 @@ Save:SI\node\ToolsMsg\Macro_SI.h
 Save:SI\node\ToolsMsg\Macro_vim.h
 
 
-/***********************************************************************/
 
-[6.1] 禅道 展讯 MTK
+
+
+[7.1] 禅道 展讯 MTK
 //1) 禅道:
 http://192.168.2.252/zentao/my/
 //2) 展讯:
@@ -164,15 +200,11 @@ http://222.66.158.139:2008/cqweb/#
 https://online.mediatek.com
 
 
-[6.2] 未整理:
-Save:SI\bak\bak_mtk_lang.h
 
-[6.3] 未改:
-Save:SI\bak\bak_mtk_weiti.h	
 
-Save:SI\bak\Macro_tmp.h
+[7.2] 参考标号:
+//参考标号:
+Save:SI\node\Test\Macro_Tmp_Node_Num.h
 
-Save:SI\bak\Macro_bak_mtk_weiti.h	
-Save:SI\bak\Macro_bak_mtk_Bengali.h 
 
 
